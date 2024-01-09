5e88ff27b298519f37e71368a260f186aae2c2d9
fatihylmz1 <fatihylmz27@icloud.com>
2024-01-09 13:06:59 +0300

    isPalindrome Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+        System.out.println("---------------------------------------");
+    }
+    public static boolean isPalindrome(int sayi) {
+        int originalSayi = sayi;
+        int tersSayi = 0;
+
+        while (sayi<0 || sayi>0) {
+            int basamak = sayi % 10;
+            tersSayi = tersSayi * 10 + basamak;
+            sayi /= 10;
+        }
+
+        return originalSayi == tersSayi;
     }
 }
